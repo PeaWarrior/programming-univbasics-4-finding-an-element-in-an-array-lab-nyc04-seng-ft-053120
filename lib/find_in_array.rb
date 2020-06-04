03d6396a405c7a74f176a25c5a5d6f4dbb81b9a9
Jackson Chen <jacksonchen171@gmail.com>
2020-06-04 21:12:28 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,8 +2,6 @@ def find_element_index(array, value_to_find)
   count = 0
   while array[count] do
     if array[count] == value_to_find
-      count += 1
-    else
       return count
     end
   end
